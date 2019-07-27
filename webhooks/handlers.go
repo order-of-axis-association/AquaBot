c097590c453c747994bc905875745bf1abff8910
Yuto Takamoto <scarletjaeger@gmail.com>
2019-07-27 10:35:06 +0000

    Add github webhook integration

---
+++ webhooks/handlers.go
@@ -0,0 +1,80 @@
+package webhooks
+
+import (
+	"fmt"
+	"strconv"
+
+	"gopkg.in/go-playground/webhooks.v5/github"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/remiscarlet/shorturl/gitio"
+)
+
+// Guild ID =>
+//   Channel ID =>
+//     Repos to announce about
+var AlertConfig = map[string]map[string][]string{
+	"604536071891714069": map[string][]string{
+		"604599071063408680": []string{
+			"test-repo",
+			"AquaBot",
+		},
+	},
+}
+
+func handlePushPayload(push github.PushPayload, dg *discordgo.Session) {
+	sender_info := push.Sender
+	sender_login := sender_info.Login
+
+	commits := push.Commits
+	num_commits := strconv.Itoa(len(commits)) + " commit"
+	if len(commits) > 1 {
+		num_commits = num_commits + "s"
+	}
+
+	repository := push.Repository
+	full_repo_name := repository.FullName
+	repo_name := repository.Name
+
+	message := sender_login + " just pushed " + num_commits + " to *" + full_repo_name + "*\n"
+
+	shortener := gitio.New()
+
+	for _, commit := range commits {
+		short_sha := commit.ID[:7]
+		short_url_bytes, err := shortener.Shorten(commit.URL)
+		short_url := string(short_url_bytes)
+
+		if err != nil {
+			fmt.Println("Error while shortening github commit url:", err)
+		}
+
+		message = message +
+				"- `" + short_sha + "`"+": "+"`" + commit.Message + "` (" + short_url + ")\n"
+	}
+
+	message = message + "\n Don't mind me. I'm just a useless goddess anyway. :("
+
+	for _, channel_config := range AlertConfig {
+		for channel_id, enabled_repos := range channel_config {
+
+			valid_repo := false
+
+			for _, enabled_repo := range enabled_repos {
+				if enabled_repo == repo_name {
+					valid_repo = true
+				}
+			}
+
+			if valid_repo {
+				//c, err := dg.State.Channel(channel_id)
+
+				//if err != nil {
+				//	fmt.Println("Error while creating channel obj:", err)
+				//}
+
+				dg.ChannelMessageSend(channel_id, message)
+			}
+		}
+	}
+}
---
+++ webhooks/listener.go
@@ -0,0 +1,43 @@
+package webhooks
+
+import (
+    "fmt"
+	"os"
+
+    "net/http"
+
+	"github.com/bwmarrin/discordgo"
+
+    "gopkg.in/go-playground/webhooks.v5/github"
+)
+
+func InitWebhookServer(dg *discordgo.Session) {
+	webhook_secret := os.Getenv("WEBHOOK_SERVER_SECRET")
+	fmt.Println(webhook_secret)
+    hook, _ := github.New(github.Options.Secret(webhook_secret))
+
+
+    const (
+        path = "/"
+    )
+
+    http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+        payload, err := hook.Parse(r, github.PushEvent)
+        if err != nil {
+            if err == github.ErrEventNotFound {
+                // ok event wasn;t one of the ones asked to be parsed
+            }
+        }
+
+        switch payload.(type) {
+	        case github.PushPayload:
+				push := payload.(github.PushPayload)
+				handlePushPayload(push, dg)
+        }
+    })
+	fmt.Println("Starting server...")
+    http.ListenAndServe(":25100", nil)
+
+	fmt.Println("Listening on port :25100")
+}
+
