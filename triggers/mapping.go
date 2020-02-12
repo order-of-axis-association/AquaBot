ecea07cf3b9b1cd2edb830081f8ad8dfe1fed930
Yuto Takamoto <scarletjaeger@gmail.com>
2020-02-12 20:55:49 +0000

    gdi I forgot a spot

--- triggers/mapping.go
+++ triggers/mapping.go
@@ -3,5 +3,5 @@ package triggers
 // I guess cap camelcase cuz global?? I use underscores everywhere else :-/
 var FuncMap = map[string]interface{}{
 	`(?i)(aqua|useless)`:        UselessAqua, // (?i) is case insensitivity
-	`(?i)<@603252075006001152>`: InvokeCleverbot,
+	`(?i)<!@603252075006001152>`: InvokeCleverbot,
 }
